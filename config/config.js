e7b065e6616bdcfac5f197a085155d45a5e80754
Velizar Harizanov <harizanovworks@gmail.com>
2020-06-14 21:05:53 +0300

    feat: database connected

--- config/config.js
+++ config/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     development: {
-        port: process.env.PORT || 3000
+        port: process.env.PORT || 3000,
+        databaseURL: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@softuni-4mwlq.gcp.mongodb.net/Cubes?retryWrites=true&w=majority`
     },
     production: {}
 };
